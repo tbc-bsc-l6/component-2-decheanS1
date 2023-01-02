63913c48b21afbbf061081c58123907db150bbee
sherpaD1 <sdechean20@tbc.edu.np>
2023-01-02 23:57:32 +0545

    laravel starter kits implemented

--- component2/database/seeders/UserSeeder.php
+++ component2/database/seeders/UserSeeder.php
@@ -14,6 +14,7 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        \App\Models\User::factory(10)->create();
+
     }
 }
--- component2/routes/web.php
+++ component2/routes/web.php
@@ -21,3 +21,13 @@ Route::get('/', [PostController::class, 'index']);
 Route::resource('posts', PostController::class);
 
 
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
